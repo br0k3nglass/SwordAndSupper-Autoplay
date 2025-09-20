b948dabfc82b99e5b8f58dc453fb9318a7c28fe6
br0k3nglass <74135172+br0k3nglass@users.noreply.github.com>
2025-09-20 16:09:07 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         SwordAndSupper Autoplay (selenium helper)
+// @name         SwordAndSupper Autoplay (selenium helper with tampermonkey integration)
 // @namespace    http://tampermonkey.net/
 // @version      0.0.7
 // @description  Automatically clicks through the map with no user prompt
