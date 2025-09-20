f0d68c2361e2adf298604e9de13a11136dbf3f09
br0k3nglass <74135172+br0k3nglass@users.noreply.github.com>
2025-09-20 10:23:30 -0600

    Update script.js

--- script.js
+++ script.js
@@ -216,7 +216,7 @@ function findAndClickStartMission() {
         $(".continue-button").click();
         // if you have maps in your inventory there will be two end-mission-buttons, otherwise there will only be one
         setTimeout(clickEndMission,500)
-        setTimeout(startMission, 2500);
+        //setTimeout(startMission, 2500);
       }
       $(".skill-button").click();
       $(".skip-button").click();
